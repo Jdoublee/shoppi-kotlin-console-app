704c62be166ceb758d0ca1ffac4a61f5b39af23a
Jdoublee <hyewon3429@gmail.com>
2023-04-05 17:40:23 +0900

    실습4-2. String Extension 함수 추가

--- src/main/kotlin/screen/ShoppingCategory.kt
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -1,5 +1,7 @@
 package screen
 
+import extensions.getNotEmptyString
+
 class ShoppingCategory {
     fun showCategories() {
         val categories = arrayOf("패션", "전자기기", "반려동물용품")
@@ -8,12 +10,12 @@ class ShoppingCategory {
         }
         println("=> 장바구니로 이동하시려면 #을 입력해주세요.")
 
-        var selectedCategory = readLine()
+        val selectedCategory = readLine().getNotEmptyString()
 
-        while (selectedCategory.isNullOrBlank()) {
-            println("값을 입력해주세요.")
-            selectedCategory = readLine()
-        }
+//        while (selectedCategory.isNullOrBlank()) {
+////            println("값을 입력해주세요.")
+////            selectedCategory = readLine()
+////        }
 
         if (selectedCategory == "#") {
             val shoppingCart = ShoppingCart()
