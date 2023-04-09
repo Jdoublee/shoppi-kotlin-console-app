1d6396e96c711daf3f4fb221d44c2698be747dcb
Jdoublee <hyewon3429@gmail.com>
2023-04-09 22:46:11 +0900

    실습5-2. ShoppingCart 에 이전화면 이동 기능 추가

--- src/main/kotlin/screen/ShoppingCart.kt
+++ src/main/kotlin/screen/ShoppingCart.kt
@@ -2,6 +2,7 @@ package screen
 
 import LINE_DIVIDER
 import data.CartItems
+import extensions.getNotEmptyString
 
 class ShoppingCart : Screen() {
     private val products = CartItems.products // 장바구니 상품 저장 싱글턴객체
@@ -27,5 +28,43 @@ class ShoppingCart : Screen() {
                 장바구니에 담긴 상품이 없습니다.
             """.trimIndent())
         }
+        showPreviousScreenOption()
+    }
+
+    private fun showPreviousScreenOption() {
+        println(
+            """
+            ${LINE_DIVIDER}
+            이전 화면으로 돌아가겠습니까? (y/n)
+        """.trimIndent()
+        )
+
+        when (readLine().getNotEmptyString()) {
+            "y" -> {
+                moveToPreviousScreen()
+            }
+            "n" -> {
+                showCartItems()
+            }
+            else -> {
+                // TODO 재입력 요청
+            }
+        }
+    }
+
+    private fun moveToPreviousScreen() {
+        ScreenStack.pop()
+        when (val previousScreen = ScreenStack.peek()) {
+            is ShoppingCategory -> {
+                previousScreen.showCategories()
+            }
+            is ShoppingProductList -> {
+                previousScreen.showProducts()
+            }
+            is ShoppingCart, is ShoppingHome, null -> {
+                // 쇼핑카트로 이동 불가. 동작 없음
+            }
+        }
+
     }
 }
